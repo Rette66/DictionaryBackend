95fe4d98815f61f5bb86aa9ec38a47d90bf0d820
Werfero <79309838+Rette66@users.noreply.github.com>
2024-08-06 16:50:12 +0800

    Delete build directory

--- build/middleware/routNotFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routeNotFound = routeNotFound;
-function routeNotFound(req, res, next) {
-    console.log('routeNotFound middleware hit');
-    const error = new Error('route not found');
-    logging.error(error);
-    return res.status(404).json({ error: error.message });
-}
